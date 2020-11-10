f64a52415404125d7686f1274769e8880db10d4b
JarAr01 <74222048+JarAr01@users.noreply.github.com>
2020-11-09 21:13:19 -0600

    Update js.js

--- JS/js.js
+++ JS/js.js
@@ -1,6 +1,6 @@
 function Calcular (){
-    var num1 = document.getElementById('entero1').value;
-    var num2 = document.getElementById('entero2').value;
+    var num1 = parseFloat(document.getElementById('entero1').value);
+    var num2 = parseFloat(document.getElementById('entero2').value);
     var sum = num1 + num2;
     document.getElementById("resultado").innerHTML="Multiplicacion : " + (num1 * num2 ) + "<br>" + "Resta : " + (num1 - num2) + "<br>" + "Suma : " + sum + "<br>" + "\nDivición : " + (num1 / num2 );
   }
